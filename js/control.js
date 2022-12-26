1ad957184ac575c98007b9d3acb6fee5169bd71b
thanhphuongIT <thanhphuong468@gmail.com>
2022-12-26 22:36:09 +0700

    feat: new code

--- js/control.js
+++ js/control.js
@@ -48,3 +48,206 @@ const count = setInterval(() => {
     seconds.text(formatTwoNumber(duration.seconds()));
   }
 }, interval);
+
+$(window)
+  .on("load resize ", function () {
+    var scrollWidth =
+      $(".tbl-content").width() - $(".tbl-content table").width();
+    $(".tbl-header").css({ "padding-right": scrollWidth });
+  })
+  .resize();
+
+//Table
+const dataCourse = [
+  {
+    id: "01",
+    name: "Android App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "02",
+    name: "IOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3848516/",
+  },
+  {
+    id: "03",
+    name: ".NET Technology",
+    url: "https://fpt-software.udemy.com/learning-paths/3848702/",
+  },
+  {
+    id: "04",
+    name: "Java Platform",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "05",
+    name: "React Native - Mobile Apps",
+    url: "https://fpt-software.udemy.com/learning-paths/4206754/",
+  },
+  {
+    id: "06",
+    name: "Low-Code Platforms",
+    url: "https://fpt-software.udemy.com/learning-paths/4141564/",
+  },
+  {
+    id: "07",
+    name: "Magento Platform",
+    url: "https://fpt-software.udemy.com/learning-paths/3958538/",
+  },
+  {
+    id: "08",
+    name: "SharePoint Platform",
+    url: "https://fpt-software.udemy.com/learning-paths/3958456/",
+  },
+  {
+    id: "09",
+    name: "Data Science: Data Analytics",
+    url: "https://fpt-software.udemy.com/learning-paths/3958352/",
+  },
+  {
+    id: "10",
+    name: "Interview and Evaluation",
+    url: "https://fpt-software.udemy.com/learning-paths/3950670/",
+  },
+  {
+    id: "11",
+    name: "Software Automation Testing",
+    url: "https://fpt-software.udemy.com/learning-paths/3848772/",
+  },
+  {
+    id: "12",
+    name: "UI/UX Design and Tools",
+    url: "https://fpt-software.udemy.com/learning-paths/3848520/",
+  },
+  {
+    id: "13",
+    name: "Interview Skills for Interviewer",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  // Tới đây
+  {
+    id: "14",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "15",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "16",
+    name: "Android App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "17",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "18",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "19",
+    name: "Android App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "20",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "21",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "21",
+    name: "Android App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "23",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "24",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "25",
+    name: "Android App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "26",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "27",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "28",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "29",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "30",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "31",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "32",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "33",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "34",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "35",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+  {
+    id: "36",
+    name: "iOS App Development",
+    url: "https://fpt-software.udemy.com/learning-paths/3503128/",
+  },
+];
+document.getElementById("table_render").innerHTML = dataCourse
+  .map(
+    (data) =>
+      `<tr>
+      <td>${data.id}</td>
+      <td>${data.name}</td>
+      <td>${data.url}</td>
+    </tr>`
+  )
+  .join("");
